6880af7e1b91d31084c33b65028d0f71f2a688c3
tanmay soin <tanmaysoin@tanmays-MacBook-Air.local>
2025-09-14 14:07:33 +0530

    Fix bcrypt issue for Render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
