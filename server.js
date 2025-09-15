0d02da8c0809dad2c488654d8eb9ec751c139e4d
tanmay soin <tanmaysoin@tanmays-MacBook-Air.local>
2025-09-15 18:53:48 +0530

    Fixed frontend path issue

--- server.js
+++ server.js
@@ -181,4 +181,5 @@ app.get("/index.html", (_req, res) => {
 });
 
 /* -------------------- START SERVER -------------------- */
-app.listen(5000, () => console.log("🚀 API running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
