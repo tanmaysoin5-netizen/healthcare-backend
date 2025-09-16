6d2b41d778627579eb63c34aca6b1aec9167c799
tanmay soin <tanmaysoin@tanmays-MacBook-Air.local>
2025-09-17 00:48:47 +0530

    frontend fixed

--- server.js
+++ server.js
@@ -185,7 +185,4 @@ app.get("/index.html", (_req, res) => {
 });
 
 /* -------------------- START SERVER -------------------- */
-const PORT = process.env.PORT || 5000;
-console.log(`Using PORT: ${PORT}`); // Log the port being used
-app.listen(PORT, () => console.log(`🚀 API running on http://localhost:${PORT}`));
 app.listen(5000, () => console.log(`🚀 API running on http://localhost:5000`));
